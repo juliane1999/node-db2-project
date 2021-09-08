4ac59daed292cd80e8e84f0d9674742597a0c231
juliane1999 <lastjuliane@gmail.com>
2021-09-07 21:54:18 -0700

    completed project

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,29 @@
-// DO YOUR MAGIC
+const express = require('express')
+const router = express.Router()
+const Cars = require('./cars-model')
+const {checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique,} = require('./cars-middleware')
+
+router.get('/', async (req, res, next) => {
+    try{
+      const data = await Cars.getAll()
+      res.json(data)
+    } catch (err) {
+      next(err)
+    }
+  })
+  
+  router.get('/:id', checkCarId, async (req, res) => {
+  res.json(req.car)
+  })
+  
+  router.post('/',checkCarPayload, checkVinNumberValid,checkVinNumberUnique, async (req, res, next) => {
+  try{
+    const car = await Cars.create(req.body)
+    res.json(car)
+  } catch (err) {
+    next(err)
+  }
+  })
+  
+  module.exports = router;
+  
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+    tbl.string('vin',17).notNullable().unique()
+    tbl.string('make',128).notNullable()
+    tbl.string('model',256).notNullable()
+    tbl.numeric('mileage').unsigned().notNullable()
+    tbl.string('title',128).notNullable()
+    tbl.string('transmission',128).notNullable()
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
